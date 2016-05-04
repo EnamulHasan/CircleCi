1a0b099bbe8351051be8883523dc48a1a566fe94
Enamul Hasan <enamul.hasan@bjitgroup.com>
2016-05-04 16:32:19 +0600

    Second commit message

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ print "hello hasan"
 print "you will be further modified"
 print "you are not allowed to come in"
 print "testing fast forwording.."
+print "testing fast forwording with second message.."
+
